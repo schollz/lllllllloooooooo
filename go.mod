981370d0ced9ec9f93fd2ce475d18352b097dc68
Zack <zack.scholl@gmail.com>
2024-02-01 06:17:02 -0800

    first commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/schollz/lllllllloooooooo
+
+go 1.21.6
+
+require github.com/schollz/logger v1.2.0
+
+require golang.org/x/exp v0.0.0-20240119083558-1b970713d09a
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	log "github.com/schollz/logger"
+	"golang.org/x/exp/io/i2c"
+)
+
+// ADS7830Read reads from the ADS7830 ADC on the specified channel.
+func ADS7830Read(ch int) (val int, err error) {
+	// Define the base command byte.
+	commandByte := byte(0b10000100)
+
+	// Select the channel.
+	switch ch {
+	case 1:
+		commandByte = 0b11000100
+	case 2:
+		commandByte = 0b10010100
+	case 3:
+		commandByte = 0b11010100
+	case 4:
+		commandByte = 0b10100100
+	case 5:
+		commandByte = 0b11100100
+	case 6:
+		commandByte = 0b11000100
+	case 7:
+		commandByte = 0b11110100
+	default:
+		return 0, fmt.Errorf("invalid channel")
+	}
+
+	d, err := i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, 0x48)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	err = d.Write([]byte{commandByte})
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	// read the byte
+	buf := make([]byte, 1)
+	err = d.Read(buf)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	// print out binary value
+	log.Debugf("Read: %08b", buf[0])
+
+	return
+}
+
+func main() {
+	log.SetLevel("debug")
+	for {
+		_, err := ADS7830Read(0)
+		if err != nil {
+			log.Error(err)
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+
+}
